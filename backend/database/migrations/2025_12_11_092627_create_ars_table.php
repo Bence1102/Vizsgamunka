3a3962015406af0368e5e9b91b4b0efbef55fbc3
Bence1102 <szente.bence04@gmail.com>
2025-12-11 10:27:33 +0100

    migrations

--- backend/database/migrations/2025_12_11_092627_create_ars_table.php
+++ backend/database/migrations/2025_12_11_092627_create_ars_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('_modositas', function (Blueprint $table) {
+        Schema::create('ars', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('_modositas');
+        Schema::dropIfExists('ars');
     }
 };
